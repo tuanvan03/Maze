df8e08b970ed234a05343b8dbe21d32beb6ef29f
thuan29012003 <dovanthuan29012003@gmail.com>
2024-05-15 20:24:17 +0700

    Them chay thuat toan

--- analyze_maze.py
+++ analyze_maze.py
@@ -5,15 +5,10 @@ from dijkstra_class import *
 from bidirectional_class import *
 import cv2
 import time
-import resource
-from bfs_class import BFS
-from dfs_class import DFS
-from astar_class import AStar
-from dijkstra_class import Dijkstra
-from bidirectional_class import Bidirectional
 from insert_maze import *
 import os
-
+import psutil
+import pandas as pd
 
 # Doc anh
 def read_image(image_path):
@@ -42,24 +37,50 @@ def read_image(image_path):
     return result
 
 
-def calculate_time_and_space_complexity(algorithm, image):
-    start_time = time.time()
-    start_memory_usage = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
-    result = algorithm(image)
-    end_time = time.time()
-    end_memory_usage = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
-    execution_time = end_time - start_time
-    memory_usage = end_memory_usage - start_memory_usage
-    return execution_time, memory_usage
-
-
 def main():
-    image_path = "D:\\SV\\HK6\\Algorithms and Analysis\\Maze\\SourceImg\\Maze\\New"
+    image_path = "C:\\Daihoc\\Ky2nam3\\TK_DGTT\\Baitaplon\\Maze\\SourceImg\\Maze\\New"
     binary_img = read_image(image_path)
-
-    for img in binary_img:
+    count = 0
+    for matrix in binary_img:
+        count += 1
+        data = []
+        print(matrix)
         start = (2, 0)
         end = (104, 58)
+        wall_pos = []
+        for j in range(104):
+            for i in range(60):
+                if (matrix[i][j] == 0):
+                    wall_pos.append((j+2, i+2))
+
+        bfs = BreadthFirst(app=None, start_node_x=start[0], start_node_y=start[1], 
+                            end_node_x=end[0], end_node_y=end[1], wall_pos=wall_pos)
+        print("Route_found : ",bfs.route_found)
+
+        start_time = time.time()
+        start_memory_usage = psutil.Process(os.getpid()).memory_info().rss / (1024 ** 2)
+        bfs.bfs_execute()
+        end_time = time.time()
+        end_memory_usage = psutil.Process(os.getpid()).memory_info().rss / (1024 ** 2)
+        
+        # Tính toán thời gian và bộ nhớ
+        execution_time = end_time - start_time
+        memory_usage = end_memory_usage - start_memory_usage
+        data.append({'Name algorithm' : "BFS" ,'Runtime (s)':  execution_time, 'Memory Usage (MB)': memory_usage, "Maze" : "maze"+str(count)})
+        try:
+            existing_df = pd.read_excel('performance_data.xlsx')
+        except FileNotFoundError:
+            existing_df = pd.DataFrame()
+
+        # Nối dữ liệu mới vào DataFrame hiện có
+        df = pd.concat([existing_df, pd.DataFrame(data)], ignore_index=True)
+        # Ghi lại DataFrame vào file Excel
+        df.to_excel('performance_data.xlsx', index=False)
+
+        print("Route_found : ",bfs.route_found)
+        print("Thời gian thực thi:", execution_time)
+        print("Bộ nhớ cần dùng:", memory_usage)
+        
 
 
 if __name__ == "__main__":
