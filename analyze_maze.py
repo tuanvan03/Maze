24b0481a72e348a5da255e2c3c981f8d26f81bc8
Tuấn Đoàn Văn <doantuanvan2003@gmail.com>
2024-05-16 08:38:49 +0700

    fix time and space complexity, add movements

--- analyze_maze.py
+++ analyze_maze.py
@@ -9,6 +9,8 @@ from insert_maze import *
 import os
 import psutil
 import pandas as pd
+from settings import *
+
 
 # Doc anh
 def read_image(image_path):
@@ -38,49 +40,66 @@ def read_image(image_path):
 
 
 def main():
-    image_path = "C:\\Daihoc\\Ky2nam3\\TK_DGTT\\Baitaplon\\Maze\\SourceImg\\Maze\\New"
+    image_path = "D:\\SV\\HK6\\Algorithms and Analysis\\Maze\\SourceImg\\Maze\\New"
     binary_img = read_image(image_path)
     count = 0
     for matrix in binary_img:
+        # print(len(matrix))
         count += 1
         data = []
         print(matrix)
-        start = (2, 0)
-        end = (104, 58)
-        wall_pos = []
+        start = (2, 4)
+        end = (105, 59)
+        wall_pos = wall_nodes_coords_list.copy()
         for j in range(104):
             for i in range(60):
-                if (matrix[i][j] == 0):
-                    wall_pos.append((j+2, i+2))
+                if matrix[i][j] == 0:
+                    wall_pos.append((j + 2, i + 2))
 
-        bfs = BreadthFirst(app=None, start_node_x=start[0], start_node_y=start[1], 
-                            end_node_x=end[0], end_node_y=end[1], wall_pos=wall_pos)
-        print("Route_found : ",bfs.route_found)
+        bfs = BreadthFirst(
+            app=None,
+            start_node_x=start[0],
+            start_node_y=start[1],
+            end_node_x=end[0],
+            end_node_y=end[1],
+            wall_pos=wall_pos,
+        )
+        print("Route_found : ", bfs.route_found)
 
+        start_memory_usage = psutil.Process(os.getpid()).memory_info().rss / (1024**2)
         start_time = time.time()
-        start_memory_usage = psutil.Process(os.getpid()).memory_info().rss / (1024 ** 2)
+
         bfs.bfs_execute()
+
         end_time = time.time()
-        end_memory_usage = psutil.Process(os.getpid()).memory_info().rss / (1024 ** 2)
-        
+        end_memory_usage = psutil.Process(os.getpid()).memory_info().rss / (1024**2)
+
         # Tính toán thời gian và bộ nhớ
         execution_time = end_time - start_time
         memory_usage = end_memory_usage - start_memory_usage
-        data.append({'Name algorithm' : "BFS" ,'Runtime (s)':  execution_time, 'Memory Usage (MB)': memory_usage, "Maze" : "maze"+str(count)})
+        data.append(
+            {
+                "Name algorithm": "BFS",
+                "Runtime (s)": execution_time,
+                "Memory Usage (MB)": memory_usage,
+                "Movement": len(bfs.route),
+                "Maze": "maze" + str(count),
+            }
+        )
         try:
-            existing_df = pd.read_excel('performance_data.xlsx')
+            existing_df = pd.read_excel("performance_data.xlsx")
         except FileNotFoundError:
             existing_df = pd.DataFrame()
 
         # Nối dữ liệu mới vào DataFrame hiện có
         df = pd.concat([existing_df, pd.DataFrame(data)], ignore_index=True)
         # Ghi lại DataFrame vào file Excel
-        df.to_excel('performance_data.xlsx', index=False)
+        df.to_excel("performance_data.xlsx", index=False)
 
-        print("Route_found : ",bfs.route_found)
+        print("Route_found : ", bfs.route_found)
         print("Thời gian thực thi:", execution_time)
         print("Bộ nhớ cần dùng:", memory_usage)
-        
+        print("Bộ nhớ cần dùng:", len(bfs.route))
 
 
 if __name__ == "__main__":
--- bfs_class.py
+++ bfs_class.py
@@ -1,7 +1,10 @@
 from settings import *
 
-class BreadthFirst():
-    def __init__(self, app, start_node_x, start_node_y, end_node_x, end_node_y, wall_pos):
+
+class BreadthFirst:
+    def __init__(
+        self, app, start_node_x, start_node_y, end_node_x, end_node_y, wall_pos
+    ):
         self.app = app
         self.start_node_x = start_node_x
         self.start_node_y = start_node_y
@@ -12,21 +15,35 @@ class BreadthFirst():
         self.route = None
         self.route_found = False
 
-    # def draw_all_paths(self, i, j):
-    #     ##### Draw each node the computer is visiting as it is searching SIMULTNEOUSLY
-    #     pygame.draw.rect(self.app.screen, TAN, (i * 12 + 240, j * 12, 12, 12), 0)
+    def draw_all_paths(self, i, j):
+        ##### Draw each node the computer is visiting as it is searching SIMULTNEOUSLY
+        pygame.draw.rect(self.app.screen, TAN, (i * 12 + 240, j * 12, 12, 12), 0)
 
-    #     ##### Redraw start/end nodes on top of all routes
-    #     pygame.draw.rect(self.app.screen, TOMATO, (240 + self.start_node_x * 12, self.start_node_y * 12, 12, 12), 0)
-    #     pygame.draw.rect(self.app.screen, ROYALBLUE, (240 + self.end_node_x * 12, self.end_node_y * 12, 12, 12), 0)
+        ##### Redraw start/end nodes on top of all routes
+        pygame.draw.rect(
+            self.app.screen,
+            TOMATO,
+            (240 + self.start_node_x * 12, self.start_node_y * 12, 12, 12),
+            0,
+        )
+        pygame.draw.rect(
+            self.app.screen,
+            ROYALBLUE,
+            (240 + self.end_node_x * 12, self.end_node_y * 12, 12, 12),
+            0,
+        )
 
-    #     # Redraw grid (for aesthetic purposes lol)
-    #     for x in range(104):
-    #         pygame.draw.line(self.app.screen, ALICE, (GS_X + x * 12, GS_Y), (GS_X + x * 12, GE_Y))
-    #     for y in range(60):
-    #         pygame.draw.line(self.app.screen, ALICE, (GS_X, GS_Y + y * 12), (GE_X, GS_Y + y * 12))
+        # Redraw grid (for aesthetic purposes lol)
+        for x in range(104):
+            pygame.draw.line(
+                self.app.screen, ALICE, (GS_X + x * 12, GS_Y), (GS_X + x * 12, GE_Y)
+            )
+        for y in range(60):
+            pygame.draw.line(
+                self.app.screen, ALICE, (GS_X, GS_Y + y * 12), (GE_X, GS_Y + y * 12)
+            )
 
-    #     pygame.display.update()
+        pygame.display.update()
 
     def checkValid(self, move):
         if move not in self.wall_pos and move not in self.visited:
@@ -41,40 +58,38 @@ class BreadthFirst():
 
     def bfs_execute(self):
         queue = [(self.start_node_x, self.start_node_y)]
-        moves_queue = ['']
-        first_out = ''
-        first_moves = ''
+        moves_queue = [""]
+        first_out = ""
+        first_moves = ""
 
         while len(queue) > 0:
             # Parent variables of parent nodes at the given time
             first_out = queue.pop(0)
             first_moves = moves_queue.pop(0)
-            for m in ['L', 'R', 'U', 'D']:
+            for m in ["L", "R", "U", "D"]:
                 i, j = first_out
-                # print('parent:', i, j)
-                if m == 'L':
+                # print("parent:", i, j)
+                if m == "L":
                     i -= 1
-                elif m == 'R':
+                elif m == "R":
                     i += 1
-                elif m == 'U':
+                elif m == "U":
                     j -= 1
-                elif m == 'D':
+                elif m == "D":
                     j += 1
 
                 # Make new variable "latest_moves" for adding onto the queue again, because you don't want the 'parent' variable to change
                 latest_moves = first_moves + m
                 if self.checkValid((i, j)):
-                    #self.draw_all_paths(i, j)
+                    # self.draw_all_paths(i, j)
                     queue.append((i, j))
                     moves_queue.append(latest_moves)
 
                 if self.findEnd((i, j)):
                     self.route = latest_moves
+                    # print(self.route)
                     self.route_found = True
                     break
-            
+
             if self.route_found:
                 break
-
-    
-
